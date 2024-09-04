7a3df6f11a31499517086d2147b1d51d93db848a
Joanna Hulek <joanna.hulek@gmail.com>
2024-09-04 12:39:13 +0200

    Touchable effects

--- Playground/App.js
+++ Playground/App.js
@@ -1,23 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight, TouchableNativeFeedback, View
+} from 'react-native';
 
 export default function App() {
-  const handlePress = () => console.log('*click!*')
+  const handlePress = (log: string) => console.log(log)
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>
-        Hello World!
-      </Text>
       {/*<Image source={require('./assets/icon.png')}/>*/}
+      <TouchableWithoutFeedback onPress={
+        ()=> console.log("TouchableWithoutFeedback")
+        // handlePress("TouchableWithoutFeedback")
+      }>
       <Image
           source={{
         width: 200,
         height: 300,
         uri: 'https://picsum.photos/200/300'}}
       />
-      <Text>
+      </TouchableWithoutFeedback>
+      <Text onPress={
+        ()=> console.log("Hello World!")
+      }>
+        Hello World!
+      </Text>
+      <TouchableNativeFeedback
+          onPress={
+              ()=> console.log("It works only with View component! And it's only an Android Component")
+          }
+      >
+        <View style={{width:200, height: 300, backgroundColor:"pink"}}>
+          <Image
+              source={{
+                width: 180,
+                height: 280,
+                uri: 'https://picsum.photos/180/280'}}
+          />
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableOpacity onPress={
+        ()=> console.log("TouchableOpacity")
+      }>
+        <Image
+            source={{
+              width: 200,
+              height: 300,
+              uri: 'https://picsum.photos/201/301'}}
+        />
+      </TouchableOpacity>
+      <Text onPress={
+        ()=> console.log("Have a nice day!")
+      }>
         Have a nice day!
       </Text>
+      <TouchableHighlight onPress={
+        ()=> console.log("TouchableHighlight")
+      }>
+        <Image
+            source={{
+              width: 200,
+              height: 300,
+              uri: 'https://picsum.photos/199/299'}}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
