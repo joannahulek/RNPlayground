ac75ac9595c683c808e5f20333422cdc9407cf8f
Joanna Hulek <joanna.hulek@gmail.com>
2024-09-05 10:52:19 +0200

    Absolute and Relative Positioning

--- Playground/App.js
+++ Playground/App.js
@@ -159,6 +159,46 @@ export default function App() {
         </>;
     }
 
+    function objectsPositioning() {
+        return <View style={{
+            backgroundColor: "white",
+            width: "90%",
+            flex: 0.95,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            // justifyContent: "space-around",
+
+        }}>
+            <View style={{
+                backgroundColor: "dodgerblue",
+                width: 100,
+                height: 100,
+            }}/>
+            <View style={{
+                backgroundColor: "green",
+                width: 100,
+                height: 100,
+                bottom: 20,
+                right: 20,
+                position: "absolute",
+            }}/>
+            <View style={{
+                backgroundColor: "yellow",
+                width: 100,
+                height: 100,
+                top: 20,
+                left: 20,
+                position: "relative",
+            }}/>
+            <View style={{
+                backgroundColor: "tomato",
+                width: 100,
+                height: 100,
+            }}/>
+        </View>;
+    }
+
     return (
     <SafeAreaView
         style={[styles.container, mainStyle]}
@@ -166,8 +206,7 @@ export default function App() {
         <StatusBar style="auto"/>
 
 
-
-
+        {objectsPositioning()}
         {/*{viewsStylesAndFlexibility()}*/}
         {/*{touchableComponents()}*/}
         {/*{buttonsAndAlerts()}*/}
